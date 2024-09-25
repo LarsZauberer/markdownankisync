7b09e2ecccfacd3b40badb1dab21a5209bf1f957
LarsZauberer <wasser.ian@gmail.com>
2024-09-25 08:42:30 +0200

    Reading markdown files

--- src/file_manager.rs
+++ src/file_manager.rs
@@ -2,15 +2,10 @@ use crate::anki::Card;
 use std::fs;
 
 fn get_md_files_in_directory(directory: &str) -> Vec<String> {
-    let read_dir_res = fs::read_dir(directory);
-    if read_dir_res.is_err() {
-        // Couldn't find any files in this directory
-        println!("Error, couldn't read wiki path");
-        return Vec::new();
-    }
+    let read_dir_res = fs::read_dir(directory).expect("Couldn't read directory specified");
 
     let mut res: Vec<String> = Vec::new();
-    for i in read_dir_res.unwrap() {
+    for i in read_dir_res {
         let path = i.unwrap().path();
         // Check if mark down file
         if path.is_file() && path.extension().unwrap() == "md" {
@@ -21,6 +16,8 @@ fn get_md_files_in_directory(directory: &str) -> Vec<String> {
     res
 }
 
-fn read_file(markdown_file: &str) -> String {}
+fn read_file(markdown_file: &str) -> String {
+    fs::read_to_string(markdown_file).expect("Should have been able to read the file")
+}
 
 fn get_cards_from_content(content: &str) -> Vec<Card> {}
