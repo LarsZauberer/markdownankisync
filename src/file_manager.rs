016b5034badd5e78149a3183593a5c73e165cbd1
LarsZauberer <wasser.ian@gmail.com>
2024-10-01 19:35:22 +0200

    Added some prototype regex expressions for the card creation

--- src/file_manager.rs
+++ src/file_manager.rs
@@ -35,7 +35,24 @@ fn get_basic_from_content(content: &str) -> Vec<Card> {
     // The regex captures all headings with a #card tag and goes to the next heading or to the end
     // of the file.
     // The id of the card is in an html comment tag -> <!--id:1234-->
-    let re = Regex::new(r"").unwrap();
+    // There are 5 capturing groups.
+    // 1: Everything before the card
+    // 2: The Card Front
+    // 3: The Card Back
+    // 4: The id (Might be empty)
+    // 5: The next heading (should stay)
+    let re = Regex::new(r"((?:.*\n)*?)#+ (.+) #card\n\n((?:.*\n)*?)(?:<!--id:(\d+)-->\n\n)?(#+)")
+        .unwrap();
+
+    // This regex captures the last card at the end of the file which is not accepted by the other
+    // regex.
+    // It has 4 capturing groups
+    // 1: Everything before the card
+    // 2: The Card Front
+    // 3: The Card Back
+    // 4: The id (Might be empty)
+    let re2 = Regex::new(r"((?:.*\n)*?)#+ (.+) #card\n\n((?:.*\n)*.*?)(?:(?:<!--id:(\d*)-->)|\z)")
+        .unwrap();
 
     // Result card
     let mut res: Vec<Card> = Vec::new();
